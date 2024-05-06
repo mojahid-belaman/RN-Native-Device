abc7ee2fbf39ffe4d75828253568819b251ce401
mojahid-belaman <mojahidbelaman@gmail.com>
2024-05-06 07:33:53 +0100

    create screen DetailsPlace

---
+++ screens/DetailsPlace.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function DetailsPlaceScreen() {
+  return (
+    <View>
+      <Text>Details Place!</Text>
+    </View>
+  );
+}
+
+export default DetailsPlaceScreen;
