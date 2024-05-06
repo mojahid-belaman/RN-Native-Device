c0e3f42f397eb46ceecb03e078ff4f17aad49835
mojahid-belaman <mojahidbelaman@gmail.com>
2024-05-06 07:36:08 +0100

    create global style of screen

---
+++ styles/screens.ts
@@ -0,0 +1,11 @@
+import { Colors } from "./colors";
+
+export const styleScreens = {
+  headerStyle: {
+    backgroundColor: Colors.primary500,
+  },
+  headerTintColor: Colors.gray700,
+  contentStyle: {
+    backgroundColor: Colors.gray700,
+  },
+};
