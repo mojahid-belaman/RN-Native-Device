d211133d776b917f0f0c900653c3480942173971
mojahid-belaman <mojahidbelaman@gmail.com>
2024-05-08 12:21:09 +0100

    handle locate user and show map with mark on the map and save after that to switch another screen

--- screens/Map.tsx
+++ screens/Map.tsx
@@ -1,28 +1,62 @@
+import { useCallback, useLayoutEffect, useState } from "react";
+import { Alert, StyleSheet, View } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { StyleSheet, View } from "react-native";
-import MapView, { MapPressEvent } from "react-native-maps";
-import { RootStackParamList } from "../types/screens";
+import MapView, { MapPressEvent, Marker } from "react-native-maps";
+
+import IconBtn from "../components/ui/IconBtn";
+import { RootStackParamList, TCoordinate } from "../types";
 
 function MapScreen({
-  route,
+  navigation,
 }: NativeStackScreenProps<RootStackParamList, "mapView">) {
+  const [coordinate, setCoordinate] = useState<TCoordinate>();
+
   const initialRegion = {
-    latitude: route.params.lat,
-    longitude: route.params.lon,
+    latitude: 32.01533845062779,
+    longitude: -6.557426233284457,
     latitudeDelta: 0.9,
     longitudeDelta: 0.9,
   };
 
   function setMarkHandler(event: MapPressEvent) {
-    
+    const latitude = event.nativeEvent.coordinate.latitude;
+    const longitude = event.nativeEvent.coordinate.longitude;
+    setCoordinate({ latitude, longitude });
   }
+
+  const saveMarkerHandler = useCallback(() => {
+    if (!coordinate) {
+      Alert.alert(
+        "No Location Picked",
+        "You have to pick a location by tapping on the map first!"
+      );
+      return;
+    }
+    navigation.navigate("newPlace", coordinate);
+  }, [navigation, coordinate]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: ({ tintColor }) => (
+        <IconBtn
+          name="save"
+          color={tintColor}
+          size={24}
+          onPress={saveMarkerHandler}
+        />
+      ),
+    });
+  }, [navigation, saveMarkerHandler]);
+
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
         initialRegion={initialRegion}
         onPress={setMarkHandler}
-      />
+      >
+        {coordinate && <Marker coordinate={coordinate} />}
+      </MapView>
     </View>
   );
 }
