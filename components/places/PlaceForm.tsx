b37233b1c00db20f778f1ccb72cc624294291722
mojahid-belaman <mojahidbelaman@gmail.com>
2024-05-11 16:25:07 +0100

    update: got all state of form place and create a new place with store it in a global state

--- components/places/PlaceForm.tsx
+++ components/places/PlaceForm.tsx
@@ -1,16 +1,50 @@
-import { useState } from "react";
+import { useCallback, useContext, useState } from "react";
 import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
 
 import ImagePicker from "./ImagePicker";
 import LocationPicker from "./LocationPicker";
 import { Colors } from "../../styles/colors";
+import PrimaryButton from "../ui/Button";
+import { RootStackParamList, TCoordinate } from "../../types";
+import { Place } from "../../models/place";
+import { PlaceContext } from "../../store/place-context";
 
 function PlaceForm() {
   const [enteredTitle, setEnteredTitle] = useState<string>("");
+  const [takeImage, setTakeImage] = useState<string>("");
+  const [pickLocation, setPickLocation] = useState<TCoordinate>();
+  const [address, setAddress] = useState<string>("");
+  const { addPlace } = useContext(PlaceContext);
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
 
   function chnageTitleHandler(value: string) {
     setEnteredTitle(value);
   }
+
+  function changeImageHandler(takenImage: string) {
+    setTakeImage(takenImage);
+  }
+
+  const changeLocationHandler = useCallback(
+    (pickedLocation: TCoordinate, address: string) => {
+      setPickLocation(pickedLocation);
+      setAddress(address);
+    },
+    []
+  );
+
+  async function addPlaceHandler() {
+    const place = new Place(enteredTitle, takeImage, address, pickLocation!);
+    addPlace(place);
+    navigation.navigate("favoritePlace");
+  }
+
+  const placeIsValid =
+    enteredTitle.length > 0 &&
+    takeImage.length > 0 &&
+    pickLocation! &&
+    address.length > 0;
 
   return (
     <ScrollView style={styles.form}>
@@ -23,8 +57,15 @@ function PlaceForm() {
           onChangeText={chnageTitleHandler}
         />
       </View>
-      <ImagePicker />
-      <LocationPicker />
+      <ImagePicker onTakeImage={changeImageHandler} />
+      <LocationPicker onPickLocation={changeLocationHandler} />
+      <PrimaryButton
+        type="fill"
+        disabled={!placeIsValid}
+        onPress={addPlaceHandler}
+      >
+        Add Place
+      </PrimaryButton>
     </ScrollView>
   );
 }
