2151a955851556a023c087f84e65eab32b3f6faa
mojahid-belaman <mojahidbelaman@gmail.com>
2024-05-11 16:27:33 +0100

    update: create a function handle navigate to DetailPlace when press on place with pass specific info of place to props to display info of it

--- components/places/PlacesList.tsx
+++ components/places/PlacesList.tsx
@@ -1,15 +1,25 @@
-import { FlatList } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
 import PlaceItem from "./PlaceItem";
 import EmptyPlace from "./EmptyPlace";
+import { useContext, useEffect } from "react";
+import { PlaceContext } from "../../store/place-context";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { RootStackParamList } from "../../types";
 
-interface IPlacesListProps {
-  places: Place[];
-}
+function PlacesList() {
+  const { places } = useContext(PlaceContext);
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
 
-function PlacesList(props: IPlacesListProps) {
-  const { places } = props;
+  function selectPlaceHandler(id: string) {
+    const findPlace = places.find((place) => place.id === id);
+    if (findPlace) {
+      navigation.navigate("detailsPlace", {
+        place: findPlace,
+      });
+    }
+  }
 
-  function selectPlaceHandler() {}
+  useEffect(() => {}, []);
 
   if (!places || places.length === 0) {
     return <EmptyPlace />;
@@ -17,13 +27,23 @@ function PlacesList(props: IPlacesListProps) {
 
   return (
     <FlatList
+      style={styles.lists}
       data={places}
       keyExtractor={({ id }) => id}
       renderItem={({ item }) => (
-        <PlaceItem place={item} onSelectPlace={selectPlaceHandler} />
+        <PlaceItem
+          place={item}
+          onSelectPlace={selectPlaceHandler.bind(null, item.id)}
+        />
       )}
     />
   );
 }
 
 export default PlacesList;
+
+const styles = StyleSheet.create({
+  lists: {
+    margin: 24,
+  },
+});
