27a8825ab6a9095db5253fbd6f4f5381f84136eb
mojahid-belaman <mojahidbelaman@gmail.com>
2024-05-11 16:29:00 +0100

    update ui of button

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,22 +1,47 @@
-import { Pressable, StyleSheet, Text } from "react-native";
+import {
+  Pressable,
+  StyleProp,
+  StyleSheet,
+  Text,
+  TextStyle,
+  ViewStyle,
+} from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Colors } from "../../styles/colors";
+
+type enumTypeBtton = "fit" | "fill";
 
 interface IButtonProps {
   children: React.ReactNode;
   iconName?: keyof typeof Ionicons.glyphMap;
+  type: enumTypeBtton;
+  disabled?: boolean;
   onPress: () => void;
 }
 
 function PrimaryButton(props: IButtonProps) {
-  const { children, iconName, onPress } = props;
+  const { children, iconName, onPress, type, disabled } = props;
+
+  let styleBtn: StyleProp<ViewStyle> = [styles.container];
+  let styleText: StyleProp<TextStyle> = [styles.text];
+
+  if (type === "fill") {
+    styleBtn.push(styles.fill);
+    styleText.push(styles.textFill);
+  }
+
   return (
     <Pressable
-      style={({ pressed }) => [styles.container, pressed && styles.pressed]}
+      style={({ pressed }) => [
+        ...styleBtn,
+        pressed && styles.pressed,
+        disabled && styles.disable,
+      ]}
+      disabled={disabled}
       onPress={onPress}
     >
       <Ionicons name={iconName} color={Colors.primary500} size={24} />
-      <Text style={styles.text}>{children}</Text>
+      <Text style={styleText}>{children}</Text>
     </Pressable>
   );
 }
@@ -29,7 +54,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     gap: 8,
-    margin: 4,
+    margin: 6,
     paddingVertical: 6,
     paddingHorizontal: 8,
     backgroundColor: "transparent",
@@ -42,5 +67,19 @@ const styles = StyleSheet.create({
   },
   pressed: {
     opacity: 0.7,
+  },
+  fill: {
+    backgroundColor: Colors.primary800,
+    borderWidth: 0,
+    borderRadius: 6,
+    elevation: 2,
+  },
+  textFill: {
+    color: "white",
+    fontSize: 18,
+  },
+  disable: {
+    backgroundColor: "gray",
+    opacity: 0.6,
   },
 });
