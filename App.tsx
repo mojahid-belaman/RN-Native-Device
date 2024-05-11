88427735d5d25caec8d9cce1bb55739e117eba5f
mojahid-belaman <mojahidbelaman@gmail.com>
2024-05-11 16:37:21 +0100

    update: added new screen

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ import IconBtn from "./components/ui/IconBtn";
 import { StatusBar } from "expo-status-bar";
 import MapScreen from "./screens/Map";
 import { RootStackParamList } from "./types";
+import PlaceProvider from "./store/place-context";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -16,43 +17,45 @@ export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="favoritePlace"
-          screenOptions={{
-            ...styleScreens,
-          }}
-        >
-          <Stack.Screen
-            name="favoritePlace"
-            component={FavoritePlacesScreen}
-            options={({ navigation }) => ({
-              title: "Your Favorite Places",
-              headerRight: ({ tintColor }) => (
-                <IconBtn
-                  name="add"
-                  color={tintColor}
-                  size={24}
-                  onPress={() => navigation.navigate("newPlace")}
-                />
-              ),
-            })}
-          />
-          <Stack.Screen
-            name="newPlace"
-            component={NewPlaceScreen}
-            options={{
-              title: "Add a new place",
+      <PlaceProvider>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="favoritePlace"
+            screenOptions={{
+              ...styleScreens,
             }}
-          />
-          <Stack.Screen name="detailsPlace" component={DetailsPlaceScreen} />
-          <Stack.Screen
-            name="mapView"
-            component={MapScreen}
-            options={{ title: "Map" }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+          >
+            <Stack.Screen
+              name="favoritePlace"
+              component={FavoritePlacesScreen}
+              options={({ navigation }) => ({
+                title: "Your Favorite Places",
+                headerRight: ({ tintColor }) => (
+                  <IconBtn
+                    name="add"
+                    color={tintColor}
+                    size={24}
+                    onPress={() => navigation.navigate("newPlace")}
+                  />
+                ),
+              })}
+            />
+            <Stack.Screen
+              name="newPlace"
+              component={NewPlaceScreen}
+              options={{
+                title: "Add a new place",
+              }}
+            />
+            <Stack.Screen name="detailsPlace" component={DetailsPlaceScreen} />
+            <Stack.Screen
+              name="mapView"
+              component={MapScreen}
+              options={{ title: "Map" }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PlaceProvider>
     </>
   );
 }
